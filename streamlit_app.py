cb7b69b6d8bb6fe8e23010a604ac3e1212eebf42
balt311 <51000749+balt311@users.noreply.github.com>
2023-01-09 22:53:37 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ def run_query(query):
         cur.execute(query)
         return cur.fetchall()
 
-rows = run_query("SELECT * from CASEGEO;")
+rows = run_query("SELECT * from dbo.CASEGEO;")
 
 # Print results.
 for row in rows:
